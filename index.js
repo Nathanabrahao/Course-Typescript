dd7b6cb4f726ba7fca465e99b3526b1b54721622
Nathanabrahao <nathanntc6@gmail.com>
2024-06-03 22:25:31 -0300

    Just 1 ° exercise

---
+++ index.js
@@ -0,0 +1,10 @@
+var firstName = "Nathan";
+var anotherName = 1;
+function gretting(name) {
+    console.log("Hello " + name);
+}
+gretting(firstName);
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(1, 2));
---
+++ index.ts
@@ -0,0 +1,18 @@
+const firstName = "Nathan"
+const anotherName = 1
+
+function gretting(name: string){
+    console.log("Hello " + name)
+}
+
+gretting(firstName)
+
+
+
+
+function soma(a:number, b:number){
+    return a + b;
+}
+
+
+console.log(soma(1, 2))
