fe33d78a2ddf9967b630ce1ee376bd5c4d72bf42
Nathanabrahao <nathanntc6@gmail.com>
2024-06-12 16:24:48 -0300

    7 °  modulo

---
+++ 7_Criação_de_tipos/index.js
@@ -0,0 +1,28 @@
+"use strict";
+//1- generics
+function showData(arg) {
+    return `O dado é: ${arg}`;
+}
+console.log(showData(5));
+console.log(showData("testando generic"));
+//2 - constrint em generic
+function showProductName(obj) {
+    return `O nome do produto é ${obj.name}`;
+}
+const myObj = { name: "Porta", cor: "Branca" };
+const otherProduct = { name: "Carro", wheels: 4, engine: 1.0 };
+console.log(showProductName(myObj));
+console.log(showProductName(otherProduct));
+const myCar = { name: "Fusca", wheels: 4, engine: 1.0 };
+const myPen = { name: "Caneta", wheels: false, engine: false };
+console.log(myCar);
+console.log(myPen);
+function showCharName(obj, name) {
+    return `${obj[name]}`;
+}
+const myChar = {
+    name: "Nathan",
+    age: 23,
+    hasDriveLicense: true
+};
+console.log(showCharName(myChar, 'name'));
---
+++ 7_Criação_de_tipos/index.ts
@@ -0,0 +1,74 @@
+//1- generics
+
+function showData<T>(arg: T): string {
+    return `O dado é: ${arg}`
+}
+
+console.log(showData(5))
+console.log(showData("testando generic"))
+
+//2 - constrint em generic
+
+function showProductName<T extends { name: string }>(obj: T) {
+    return `O nome do produto é ${obj.name}`
+}
+
+const myObj = { name: "Porta", cor: "Branca" }
+const otherProduct = { name: "Carro", wheels: 4, engine: 1.0 }
+
+console.log(showProductName(myObj))
+console.log(showProductName(otherProduct))
+
+//3 - Interface Generics
+interface MyObject<T, U> {
+    name: string
+    wheels: T
+    engine: U
+}
+
+type Car = MyObject<number, number>
+type Pen = MyObject<boolean, boolean>
+
+const myCar: Car = { name: "Fusca", wheels: 4, engine: 1.0 }
+const myPen: Pen = { name: "Caneta", wheels: false, engine: false }
+
+console.log(myCar)
+console.log(myPen)
+
+
+//4- type parameters
+
+
+/* function getSomeKey<T, K extends keyof T>(obj: T, key: K) {
+    return `A chave ${key} está presente no objeto e tem o valor de ${obj[key]}`
+}
+
+const server = {
+    hd: "2TB",
+    ram: "32GB"
+}
+
+console.log(getSomeKey(server, 'ram')) */
+
+//Comentado pois está reclamando a key mas está funcionando o código
+
+//5 - keyof type operator
+type Character = {name: string, age: number, hasDriveLicense: boolean}
+
+type C = keyof Character
+
+function showCharName(obj: Character, name: C){
+    return `${obj[name]}`
+}
+
+const myChar: Character = {
+    name: "Nathan",
+    age: 23,
+    hasDriveLicense: true
+}
+
+
+console.log(showCharName(myChar, 'name'))
+
+
+
