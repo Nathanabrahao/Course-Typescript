a6a9202e400ebaaef806d27f91c9e65d312bed6e
Nathanabrahao <nathanntc6@gmail.com>
2024-06-21 14:08:42 -0300

    Conclusão da aula 11

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 import FirstComponent from './Components/FirstComponent';
 import SecondeComponent from './Components/SecondeComponent';
-import Destructuring from './Components/Destructuring';
+import Destructuring, { Category } from './Components/Destructuring';
 import State from './Components/State';
+import Context from './Components/Context';
+
+type textOrNull = string | null;
+
+type fixed = "isso" | "aquilo" | "Ou";
+
+//9- context
+interface IAppContext {
+  language: string;
+  framework: string;
+  projects: number;
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
+
+
 
 function App() {
 
@@ -17,8 +33,23 @@ function App() {
     return `Olá, ${name}`
   }
 
+  //8 - Type
+  const myText: textOrNull = "Tem algum texto aqui"
+  let mySecondText: textOrNull = null;
+
+  mySecondText = "opa"
+
+  const testandoFixed: fixed = "Ou"
+
+
+  const contextValue: IAppContext = {
+    language: "JavaScript",
+    framework: "Express",
+    projects: 5,
+  };
 
   return (
+    <AppContext.Provider value={contextValue}>
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
@@ -32,9 +63,21 @@ function App() {
         content='Algum conteúdo'
         commentsQty={10}
         tags={["ts", "js"]}
+        category={Category.JS}
+      />
+      <Destructuring
+        title='Segundo post'
+        content='Algum conteúdo'
+        commentsQty={10}
+        tags={["ts", "js"]}
+        category={Category.TS}
       />
       <State />
+      {myText && <p>Tem texto na variavel</p>}
+      {mySecondText && <p>Tem texto na variavel</p>}
+      <Context />
     </div>
+    </AppContext.Provider>
   );
 }
 
